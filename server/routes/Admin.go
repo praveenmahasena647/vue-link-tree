80ac4dfd8ba4709bd076571a30a4e0a889dcdfb1
praveenmahasena647 <115216588+praveenmahasena647@users.noreply.github.com>
2022-11-10 23:14:15 +0530

    mhm

--- server/routes/Admin.go
+++ server/routes/Admin.go
@@ -1,15 +1,19 @@
 package routes
 
 import (
+	"context"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/praveenmahasena647/vue-link-tree/dbs"
 	jwthandle "github.com/praveenmahasena647/vue-link-tree/jwt-handle"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // this is gonna be fun i can already feel it
+// lets have JSML to gether
 
 func adminLogin(w http.ResponseWriter, r *http.Request) {
 	var Admin *dbs.AdminLogIn = &dbs.AdminLogIn{}
@@ -22,15 +26,29 @@ func adminLogin(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte(token))
 }
+
 func deactivateUser(w http.ResponseWriter, r *http.Request) {
-	var data = map[string]string{}
-
-	json.NewDecoder(r.Body).Decode(&data)
-	log.Println(data)
+	var data, dataErr = io.ReadAll(r.Body)
+	if dataErr != nil {
+		w.WriteHeader(500)
+		return
+	}
+	var _, notDone = dbs.UserCollection.UpdateOne(context.TODO(), bson.M{"": string(data)}, bson.M{"": false})
+	if notDone != nil {
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(200)
 }
-func deleteUser(w http.ResponseWriter, r *http.Request) {
-	var data = map[string]string{}
 
-	json.NewDecoder(r.Body).Decode(&data)
-	log.Println(data)
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	var data, dataErr = io.ReadAll(r.Body)
+	if dataErr != nil {
+		w.WriteHeader(500)
+		return
+	}
+	var done, notDelete = dbs.UserCollection.DeleteOne(context.TODO(), bson.M{"username": string(data)})
+	log.Println(done, notDelete)
+
+	w.WriteHeader(200)
 }
